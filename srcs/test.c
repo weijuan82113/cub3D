25eebadcc5ec6309bfcc3dd51534243e20e57b9d
kitsuki <IT.King5963@gmail.com>
2023-09-25 23:25:19 +0900

    paint colorful wall

--- srcs/test.c
+++ srcs/test.c
@@ -6,7 +6,7 @@
 /*   By: kitsuki <kitsuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 18:16:39 by wchen             #+#    #+#             */
-/*   Updated: 2023/09/25 22:53:12 by kitsuki          ###   ########.fr       */
+/*   Updated: 2023/09/25 23:23:36 by kitsuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,28 @@ static int32_t clear(t_window *tmp) {
 	return 0;
 }
 
+static int getColor(int kakudo) {
+	int map[3][5] = {{1, 1, 0, 1, 1},{1, 0, 0, 0, 1},{1, 0, 0, 0, 1}};
+	int x = 150;
+	int y = 100;
+	int i = x / 50;
+	for (int j = y / 50; j >= 0 && (0 <= i && i <= 4); j++) {
+		if (map[j][i] == 1)
+			return 0x00FFFF;
+		if (kakudo < 0)
+			i--;
+		else if (kakudo > 0)
+			i++;
+	}
+	return 0xFF0000;
+}
+
 static int repaint(t_window *tmp) {
-	for (int i = 0; i < 1000; i++)
-		mlx_pixel_put(tmp->mlx, tmp->win, 100, i, 0xFF00FF);
+	for (int i = 0; i < 1000; i++) {
+		for (int j = 0; j < 1000; j++) {
+			mlx_pixel_put(tmp->mlx, tmp->win, i, j, getColor((i - 500) / 200));
+		}
+	}
 	return 0;
 }
 
