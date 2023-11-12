e3b2787344c37bf9616064f5a2dc73302d6e43d4
kitsuki <IT.King5963@gmail.com>
2023-11-12 21:26:25 +0900

    fix error-with-free

--- srcs/mlx_initial.c
+++ srcs/mlx_initial.c
@@ -6,7 +6,7 @@
 /*   By: kitsuki <kitsuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 17:37:32 by wchen             #+#    #+#             */
-/*   Updated: 2023/11/07 23:18:06 by kitsuki          ###   ########.fr       */
+/*   Updated: 2023/11/12 21:14:10 by kitsuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	g_init(t_g_board *g_board)
 	g_board->line_lst = NULL;
 	g_board->img_lst = NULL;
 	g_board->color_lst = NULL;
-	g_board->p_position = NULL;
+	g_board->p_position = 0;
 	g_board->t_count = 0;
-	g_board->m_info -> map = NULL;
-	g_board->m_info -> h = NULL;
-	g_board->m_info -> w = NULL;
+	g_board->m_info->map = NULL;
+	g_board->m_info->h = 0;
+	g_board->m_info->w = 0;
 	g_board->identifier = NULL;
 }
 
@@ -31,22 +31,17 @@ bool	game_board_initial(t_g_board *g_board)
 	if (!g_board->m_info)
 		return (ft_error(MALLOC_ERR));
 	g_init(g_board);
-	g_board->m_info->h = (int *)malloc(sizeof(int));
-	if (!g_board->m_info->h)
-		return (ft_error(MALLOC_ERR));
-	*g_board->m_info->h = 0;
-	g_board->m_info->w = (int *)malloc(sizeof(int));
-	if (!g_board->m_info->w)
-		return (ft_error(MALLOC_ERR));
-	*g_board->m_info->w = 0;
-	g_board->p_position = (int *)malloc(sizeof(int));
-	if (!g_board->p_position)
-		return (ft_error(MALLOC_ERR));
-	*g_board->p_position = 0;
 	g_board->identifier = ft_split(IDENTIFIER, '/');
 	if (!g_board->identifier)
 		return (ft_error(MALLOC_ERR));
 	return (false);
+}
+
+static bool	free_mlx_and_return_true(t_mlx **dst)
+{
+	free_all(*dst);
+	*dst = NULL;
+	return (true);
 }
 
 bool	mlx_initial(t_mlx **dst)
@@ -59,14 +54,14 @@ bool	mlx_initial(t_mlx **dst)
 		return (true);
 	mlx->p_mlx = mlx_init();
 	if (!mlx->p_mlx)
-		return (true);
+		return (free_mlx_and_return_true(dst));
 	mlx->p_win = mlx_new_window(mlx->p_mlx, WIN_X, WIN_Y, "Cub3D");
 	if (!mlx->p_win)
-		return (true);
+		return (free_mlx_and_return_true(dst));
 	mlx->g = malloc(sizeof(t_g_board));
 	if (!mlx->g)
-		return (true);
+		return (free_mlx_and_return_true(dst));
 	if (game_board_initial(mlx->g))
-		return (true);
+		return (free_mlx_and_return_true(dst));
 	return (false);
 }
